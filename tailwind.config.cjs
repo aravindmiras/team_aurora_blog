4985a0201d4e9d5ed4dae62b90b4ed715dea25c7
aravindmiras <aravindmiras@gmail.com>
2023-12-17 15:52:49 +0530

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/components/*.astro','./src/content/*.astro','./src/layouts/*.astro','./src/pages/*.astro','./src/pages/posts/*.astro'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
